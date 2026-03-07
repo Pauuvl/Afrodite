7aaf500bac7bcd5c21c137bb747fda7a843b04e8
Pauuvl <159390787+Pauuvl@users.noreply.github.com>
2026-03-07 16:45:23 -0500

    Front

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,30 @@
 console.log("Afrodite funcionando correctamente");
+var swiper = new Swiper(".product-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".icon-arrow-right",
+    prevEl: ".icon-arrow-left",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    992: {
+      slidesPerView: 3
+    },
+    1200: {
+      slidesPerView: 4
+    }
+  }
+});
